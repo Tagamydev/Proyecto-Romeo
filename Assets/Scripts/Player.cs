5702b425dd56530cf4e64ee3ee912fe715bd2ad9
andres <andres@localhost.localdomain>
2024-10-22 00:05:18 +0200

    Mi capilla sixtina, digo mi primer blocking

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float moveSpeed =5f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +14,11 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-	    print("hello");
+	    float moveX = Input.GetAxis("Horizontal");
+	    float moveY = Input.GetAxis("Vertical");
+	    
+	    Vector3 movement = new Vector3(moveX, 0f, moveY)*moveSpeed* Time.deltaTime  ;
+	    transform.Translate(movement);
         
     }
 }
