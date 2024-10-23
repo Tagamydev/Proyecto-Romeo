51aa5e56c9fa69a7cd4fa4e6338c3bd9c8606971
samusanc <samusanc@student.42madrid.com>
2024-10-23 11:51:05 +0200

    uploading the art placeholders

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+	public	GameObject	object_usable;
+
     public float moveSpeed =5f;
     // Start is called before the first frame update
     void Start()
